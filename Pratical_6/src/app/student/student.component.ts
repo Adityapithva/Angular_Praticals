6a3fc266540341a05ec82fa136a5f71f486659ce
Adityapithva <121885084+Adityapithva@users.noreply.github.com>
2024-04-13 20:01:40 +0530

    Pratical 6

---
+++ Pratical_6/src/app/student/student.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+// Define the Student class
+class Student {
+  constructor(public firstName: string, public lastName: string, public age: number) {}
+}
+
+@Component({
+  selector: 'app-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css'] 
+})
+export class StudentComponent implements OnInit {
+  title: string = 'Student Records';
+  studentCount: number = 0;
+  students: Student[] = [];
+
+  ngOnInit() {
+    this.students.push(new Student("Aditya", "Pithva", 17));
+    this.students.push(new Student("Bhargav", "Bhatt", 17));
+    this.students.push(new Student("Darshan", "Jadav", 17));
+    this.studentCount = this.students.length;
+  }
+
+  updateTable() {
+    const currentCount = this.students.length;
+    if (this.studentCount > currentCount) {
+      for (let i = currentCount; i < this.studentCount; i++) {
+        this.students.push(new Student("", "", 0));
+      }
+    }else if(this.studentCount < currentCount){
+      this.students.splice(this.studentCount);
+    }
+  }
+  removeStudent(index:number){
+    this.students.splice(index, 1);
+  }
+}
